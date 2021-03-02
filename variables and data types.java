ecd7d600e67d71ec37fcef109e84f853c34069ac
muthudatabreak <79848842+muthudatabreak@users.noreply.github.com>
2021-03-02 11:01:29 +0530

    Update variables and data types.java

--- variables and data types.java
+++ variables and data types.java
@@ -1,21 +1,16 @@
-// https://www.w3schools.com/java/tryjava.asp?filename=demo_interface_multi
-import java.io.*;
-class program1
-{
-public static void main(String args[]) throws IOException
-{
-DataInputStream in=new DataInputStream(System.in);
-System.out.print("Enter byte value:");
-byte b1=Byte.parseByte(in.readLine());
-System.out.println("Byte Value of b1: "+b1);
-System.out.print("Enter integer value:");
-int i=Integer.parseInt(in.readLine());
-System.out.println("Integer Value:"+i);
-System.out.print("Enter float value:");
-float f=Float.parseFloat(in.readLine());
-System.out.println("Float Value:"+f);
-System.out.print("Enter String value:");
-String str=in.readLine();
-System.out.println("Char Value:"+str);
+//https://www.w3schools.com/java/tryjava.asp?filename=demo_interface_multi
+
+public class Main {
+  public static void main(String[] args) {
+int myNum = 5;               
+float myFloatNum = 5.99f;    
+char myLetter = 'D';       
+boolean myBool = true;     
+String myText = "Hello";   
+System.out.println(myText);
+System.out.println(myBool);
+System.out.println(myLetter);
+System.out.println(myFloatNum);
+System.out.println(myNum);
 }
 }
